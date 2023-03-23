3ac446d88bbd46e1b6fa9b189685805ce4eba727
ezequiel bugallo <ezequielbugallo@gmail.com>
2023-03-23 17:07:33 -0300

    primeros archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { Login } from "./Login"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Login />
+     <h1>Esto es app.</h1>
+     <h3>Probando los cambios y el pull</h3>
     </div>
   );
 }
 
 export default App;
+
